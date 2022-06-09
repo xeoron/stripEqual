1320e7d63ce5aeb6a2d90b62d034dce05dd59c31
Jason Campisi <aitsinformation@gmail.com>
2022-06-09 08:04:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# stripEqual
+ Strip the equal sign out of files in csv files by installing this script system wide
---
+++ stripEqual.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# Name: stripEqual.pl
+# Author: Jason Campisi
+# Date: 6/9/2022
+# Version 0.1.0 
+# Purpose: Strip out equal signs from csv files
+#           Works the same as this 1 linger:  perl -0777 -pe 's/=//g' ./TagListing.csv > ./result.csv 
+#           Only you can install this script system wide to use!!!
+# Usage: ./stripEqual.pl > ./results.csv
+# Repository: 
+# License: Released under GPL v3 or higher. Details here http://www.gnu.org/licenses/gpl.html
+
+
+while (<>){ 
+    $_=~s/=//ge; 
+    print "$_\n"; 
+}
+
+exit 0;
