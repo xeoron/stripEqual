a146d22e64ed8caf72cc973701d60375654c83b2
Jason Campisi <aitsinformation@gmail.com>
2022-06-13 09:52:58 -0400

    bug fix
    new line does not need adding so removed it. csv files already have \n char in them

--- stripEqual.pl
+++ stripEqual.pl
@@ -2,7 +2,7 @@
 # Name: stripEqual.pl
 # Author: Jason Campisi
 # Date: 6/9/2022
-# Version 0.1.1 
+# Version 0.1.2 
 # Purpose: Strip out equal signs from csv files
 #           Works the same as this 1 linger:  perl -0777 -pe 's/=//g' ./TagListing.csv > ./result.csv 
 #           Only you can install this script system wide to use!!!
@@ -13,7 +13,7 @@
 
 while (<>){ 
     $_=~s/=//ge; 
-    print "$_\n"; 
+    print "$_";
 }
 
 exit 0;
